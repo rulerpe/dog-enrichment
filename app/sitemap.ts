732a1ad0931f113a979a0d998a0d375683bbaf09
rulerpe <neetisthebest@gmail.com>
2024-01-16 12:54:38 -0800

    sitmap date

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,10 +2,11 @@ import { MetadataRoute } from "next";
 import activities from "./lib/activity-data";
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const lastModified = new Date().toISOString().split("T")[0];
   const homeMap: MetadataRoute.Sitemap = [
     {
       url: "https://www.scentsavvydogenrich.com",
-      lastModified: new Date(),
+      lastModified: lastModified,
       changeFrequency: "monthly",
       priority: 1,
     },
@@ -13,7 +14,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   let activityMap: MetadataRoute.Sitemap = activities.map((activity) => {
     return {
       url: `https://www.scentsavvydogenrich.com/activity/${activity.slug}`,
-      lastModified: new Date(),
+      lastModified: lastModified,
       changeFrequency: "monthly",
       priority: 0.8,
     };
